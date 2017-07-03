742932686a8669badeaa8a31702ab88399f59609
vagrant <vagrant@homestead>
2017-07-03 05:50:07 +0000

    Added test chart

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,26 @@
 
 @section('content')
 
-    <p>Hello!</p>
+    <!-- TradingView Widget BEGIN -->
+    <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+
+    <script type="text/javascript">
+        new TradingView.widget({
+            "width": 980,
+            "height": 610,
+            "symbol": "NASDAQ:AAPL",
+            "interval": "5",
+            "timezone": "Etc/UTC",
+            "theme": "Black",
+            "style": "1",
+            "locale": "en",
+            "toolbar_bg": "#f1f3f6",
+            "enable_publishing": false,
+            "allow_symbol_change": true,
+            "hideideas": true
+        });
+    </script>
+    <!-- TradingView Widget END -->
+
 
 @endsection
